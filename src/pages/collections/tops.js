589b84033d660f257daa8d8ae4d58a428ed766d9
Allan Viray <allang.v.dev@gmail.com>
2022-10-20 17:33:22 -0500

    Add a collections sub directory and a page to visit

---
+++ src/pages/collections/tops.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Tops() {
+  return (
+    <div>
+      <h1>Tops</h1>
+      <p>Buy tops here.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, rem
+        assumenda ut officia enim quidem optio ipsam. Sapiente, libero quas!
+      </p>
+    </div>
+  )
+}
