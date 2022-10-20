344f90581ef3addbe06aea93e49387adcd21e0d6
Allan Viray <allang.v.dev@gmail.com>
2022-10-20 17:40:39 -0500

    Create a base route index.js for the collections directory

---
+++ src/pages/collections/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Collections() {
+  return (
+    <div>
+      <h1>Our Collections</h1>
+      <p>We have several items to suit your style.</p>
+    </div>
+  )
+}
