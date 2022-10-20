3448f1f73c7e6a58f4b15805882334de089b9056
Allan Viray <allang.v.dev@gmail.com>
2022-10-20 18:51:38 -0500

    Create a global.css file to contain global styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Navbar from "./Navbar"
+import "../styles/global.css"
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
       <Navbar />
       <div className="content">{children}</div>
-      <footer>Copyright 2022 E-commerce Site</footer>
+      <footer>
+        <p>Copyright 2022 E-commerce Site</p>
+      </footer>
     </div>
   )
 }
