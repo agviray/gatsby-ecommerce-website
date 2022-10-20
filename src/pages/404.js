96e86667f427228e6981cc71d15f56c776c64122
Allan Viray <allang.v.dev@gmail.com>
2022-10-20 17:45:00 -0500

    Create a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Uh-oh! Looks like the page you are trying to access does not exist.</p>
+    </div>
+  )
+}
