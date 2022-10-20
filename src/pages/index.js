665229a761e7b3f3a753f91b7a08fa57d2b44d85
Allan Viray <allang.v.dev@gmail.com>
2022-10-20 18:02:38 -0500

    Create a navbar and display it on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <section>
+      <Navbar />
       <div>
         <h1>Official Online Store</h1>
         <p>Welcome to the shop!</p>
