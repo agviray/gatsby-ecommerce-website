a59278bd767c960ce4f030c93d37a6a8bbbaff3a
Allan Viray <allang.v.dev@gmail.com>
2022-10-20 17:09:33 -0500

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h1>Gatsby E-commerce Website</h1>
+        <p>Welcome to the shop!</p>
+      </div>
+    </section>
+  )
 }
