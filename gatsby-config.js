401889d6b2277ec1a6cdce5b0826d2c9d37e2d93
Allan Viray <allang.v.dev@gmail.com>
2022-10-21 12:00:22 -0500

    Edit gatsby-config.js file
    - Add siteMetadata property to module.exports object.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: "Online Store",
+    description: "online clothing store",
+    copyright: "This website is copyright 2022 Online Store",
+  },
 }
