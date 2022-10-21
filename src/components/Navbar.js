9fb76663600aea4d3b29bb5b7995a85146204b87
Allan Viray <allang.v.dev@gmail.com>
2022-10-21 12:46:19 -0500

    Make siteMetadata available in the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
 import React from "react"
 
 export default function Navbar() {
+  const data = useStaticQuery(graphql`
+    query SiteInfo {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  // - Any property of siteMetadata--ie, title--was specified in the gatsby-config.js file.
+  const { title } = data.site.siteMetadata
+
   return (
     <nav>
-      <h1>E-commerce Site</h1>
+      <h1>{title}</h1>
       <div className="links">
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>About</Link>
