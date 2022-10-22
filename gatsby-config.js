02c2bf3d1ea341354062bf6656a831aa4ae8c97e
Allan Viray <allang.v.dev@gmail.com>
2022-10-21 20:53:26 -0500

    Update gatsby-config.js file
    - Set up gatsby-transformer-remark plugin and
      gatsby-source-filesystem plugin to enable future implementation.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `categories`,
+        path: `${__dirname}/src/categories/`,
+      },
+    },
+  ],
   siteMetadata: {
     title: "Online Store",
     description: "online clothing store",
