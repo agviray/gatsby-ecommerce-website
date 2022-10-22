08f29c780da761c6411b33790a4fc6c64e446362
Allan Viray <allang.v.dev@gmail.com>
2022-10-21 21:05:39 -0500

    Update the Collections index page
    - Add content from categories markdown files by implementing the features
      that gatsby-transformer-remark and gatsby-source-filesystem plugins
      provide.

--- src/pages/collections/index.js
+++ src/pages/collections/index.js
@@ -1,14 +1,45 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/collections.module.css"
 
-export default function Collections() {
+export default function Collections({ data }) {
+  const categories = data.allMarkdownRemark.nodes
   return (
     <Layout>
       <div className={styles.collections}>
         <h2>Our Collections</h2>
         <span>We have several items to suit your style.</span>
+        <div className={styles.categories}>
+          {categories.map(category => (
+            <Link
+              to={`/collections/${category.frontmatter.slug}`}
+              key={category.id}
+            >
+              <div>
+                <h3>{category.frontmatter.title}</h3>
+                <p>{category.frontmatter.items}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// - Use query data in the component by accessing the data prop.
+export const query = graphql`
+  query Collections {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          items
+          slug
+        }
+        id
+      }
+    }
+  }
+`
