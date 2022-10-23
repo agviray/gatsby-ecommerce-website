bdd048c70aa8eb23b7b089b03410dbc536725244
Allan Viray <allang.v.dev@gmail.com>
2022-10-23 18:02:15 -0500

    Add temporary image to Home index.js page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
-export default function Home() {
+export default function Home({ data }) {
+  const image = getImage(data.image.childImageSharp)
+  // console.log(data)
   return (
     <Layout>
       <section className={styles.header}>
@@ -11,6 +15,21 @@ export default function Home() {
           <span>Welcome to the shop!</span>
         </div>
       </section>
+      <GatsbyImage image={image} alt="items group" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  query HomePage {
+    image: file(relativePath: { eq: "items-group01.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(
+          layout: FULL_WIDTH
+          placeholder: BLURRED
+          formats: [AUTO, WEBP]
+        )
+      }
+    }
+  }
+`
