15c1186b4c0a9a69a0368d87e46f512962ddaf97
Allan Viray <allang.v.dev@gmail.com>
2022-10-24 18:12:48 -0500

    Create initial Hamburger component and styles

---
+++ src/components/Hamburger.js
@@ -0,0 +1,14 @@
+import React from "react"
+import * as styles from "../styles/hamburger.module.css"
+
+const Hamburger = () => {
+  return (
+    <div className={styles.hamburger}>
+      <span />
+      <span />
+      <span />
+    </div>
+  )
+}
+
+export default Hamburger
