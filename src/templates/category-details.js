1c8ab0a78fff7195eb8aa2f809900c9f7b314ae8
Allan Viray <allang.v.dev@gmail.com>
2022-10-24 13:14:23 -0500

    Display page content in category-details.js page template

--- src/templates/category-details.js
+++ src/templates/category-details.js
@@ -1,23 +1,50 @@
 import React from "react"
+import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 import * as styles from "../styles/category-details.module.css"
 
-export default function CategoryDetails() {
+export default function CategoryDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, description, featuredImg } = data.markdownRemark.frontmatter
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>Category Type</h2>
-        <h3>Description</h3>
+        <h2>{title}</h2>
+        <h3>{description}</h3>
         <div className={styles.featured}>
           Banner Image
-          {/* 
-          Page banner image (if desired) can go here, but first need to add it to category markdown file frontmatter detail.
-           */}
-          {/* <GatsbyImage /> */}
+          <GatsbyImage
+            image={featuredImg.childImageSharp.gatsbyImageData}
+            alt={"page banner"}
+          />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query CategoryDetailsPage($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        description
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              formats: [AUTO, WEBP]
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
+    }
+  }
+`
