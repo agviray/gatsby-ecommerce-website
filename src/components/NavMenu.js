480a79d982fa1eabdd519967c0539adf8a3e96dc
Allan Viray <allang.v.dev@gmail.com>
2022-10-24 20:39:31 -0500

    Update NavMenu content and styles
    - Add links to direct product pages
    
    - Add media queries to handle responsiveness and update NavMenu as
      appropriate.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,14 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 import * as styles from "../styles/navmenu.module.css"
+import useWindowWidth from "./hooks/useWindowWidth"
 
 const NavMenu = () => {
-  return (
+  const windowWidth = useWindowWidth()
+  const menu = (
     <div className={styles.menu}>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
-      <Link to={"/collections"}>Collections</Link>
+      <Link to={"/collections/new-styles"}>New Styles</Link>
+      <Link to={"/collections/tops"}>Tops</Link>
+      <Link to={"/collections/bottoms"}>Bottoms</Link>
+      <Link to={"/collections/outerwear"}>Outerwear</Link>
+      <Link to={"/collections/accessories"}>Accessories</Link>
     </div>
+  )
+
+  return windowWidth < 800 ? (
+    <div className={styles.container}>{menu}</div>
+  ) : (
+    <>{menu}</>
   )
 }
 
