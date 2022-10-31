11b30ae5d6240d4ccf9f1e7daf197af179ff68bc
Allan Viray <allang.v.dev@gmail.com>
2022-10-31 15:53:28 -0500

    Create initial ProductLink.js component

---
+++ src/components/ProductLink.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const ProductLink = ({ image, name, price }) => {
+  ;<div className="container">
+    <a href="#">
+      <div className="image">
+        {/* <img /> tag containing product image goes here */}
+      </div>
+      <div className="caption">
+        {/* <h3>name</h3> product name goes here */}
+        {/* <span>price</span> product price goes here */}
+      </div>
+    </a>
+  </div>
+}
+
+export default ProductLink
