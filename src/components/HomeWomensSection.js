5dcdbd01543b0a08f45a86b48383fa0cf501e26d
Allan Viray <allang.v.dev@gmail.com>
2022-11-19 19:21:01 -0600

    Update image used for womens department and update all associated files

--- src/components/HomeWomensSection.js
+++ src/components/HomeWomensSection.js
@@ -11,7 +11,7 @@ const HomeWomensSection = () => {
     query HomeWomensSectionComponent {
       markdownRemark(frontmatter: { slug: { eq: "womens" } }) {
         frontmatter {
-          mainImageMobile {
+          mobileImage {
             childImageSharp {
               gatsbyImageData(
                 formats: [AUTO, WEBP]
@@ -20,23 +20,13 @@ const HomeWomensSection = () => {
               )
             }
           }
-          mainImageDesktop {
+          desktopImage {
             childImageSharp {
               id
               gatsbyImageData(
                 layout: FULL_WIDTH
                 formats: [AUTO, WEBP]
                 placeholder: BLURRED
-              )
-            }
-          }
-          supportImages {
-            childImageSharp {
-              id
-              gatsbyImageData(
-                formats: [AUTO, WEBP]
-                placeholder: BLURRED
-                layout: FULL_WIDTH
               )
             }
           }
@@ -45,48 +35,21 @@ const HomeWomensSection = () => {
     }
   `)
 
-  const { mainImageMobile, mainImageDesktop } = data.markdownRemark.frontmatter
-  const { supportImages } = data.markdownRemark.frontmatter
-
-  const renderDesktopContent = () => {
-    return (
-      <div className={styles.content}>
-        <div className={styles.mainImageContainer}>
-          <GatsbyImage
-            image={mainImageDesktop.childImageSharp.gatsbyImageData}
-            alt="woman posing"
-          />
-        </div>
-        <div className={styles.supportImagesContainer}>
-          <div className={styles.supportContainer01}>
-            <GatsbyImage
-              image={supportImages[0].childImageSharp.gatsbyImageData}
-              alt="woman wearing sunglasses"
-            />
-          </div>
-          <div className={styles.supportContainer01}>
-            <GatsbyImage
-              image={supportImages[1].childImageSharp.gatsbyImageData}
-              alt="woman skirt bottoms"
-            />
-          </div>
-          <div className={styles.womensDepartmentLink}></div>
-        </div>
-      </div>
-    )
-  }
+  const mobileImage =
+    data.markdownRemark.frontmatter.mobileImage.childImageSharp.gatsbyImageData
+  const desktopImage =
+    data.markdownRemark.frontmatter.desktopImage.childImageSharp.gatsbyImageData
 
   return (
     <>
       {windowWidth < 800 ? (
         <div>
-          <GatsbyImage
-            image={mainImageMobile.childImageSharp.gatsbyImageData}
-            alt="woman posing"
-          />
+          <GatsbyImage image={mobileImage} alt="woman posing" />
         </div>
       ) : (
-        renderDesktopContent()
+        <div className={styles.desktopImageContainer}>
+          <GatsbyImage image={desktopImage} alt="collage of images" />
+        </div>
       )}
     </>
   )
--- src/departments/womens/womens-department.md
+++ src/departments/womens/womens-department.md
@@ -1,11 +1,7 @@
 ---
 position: 1
-mainImageMobile: ../images/home-womens-feat-img-mobile.jpg
-mainImageDesktop: ../images/home-womens-feat-img-desktop.jpg
-supportImages: [
-../images/home-womens-feat-img-support01.jpg, 
-../images/home-womens-feat-img-support02.jpg
-]
+mobileImage: ../images/home-womens-feat-img-mobile.jpg
+desktopImage: ../images/home-womens-collage.png
 department: womens
 name: Women's Department
 slug: womens
