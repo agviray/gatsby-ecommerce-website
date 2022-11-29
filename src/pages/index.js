8f53e92980079d9c9347bb7e84edccf2bcbca94a
Allan Viray <allang.v.dev@gmail.com>
2022-11-28 19:03:31 -0600

    Link home page sections to appropriate departments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { section, innerContainer, text } from "../styles/home.module.css"
 import HomeWomensSection from "../components/HomeWomensSection"
 import HomeMensSection from "../components/HomeMensSection"
 import HomeHomeGoodsSection from "../components/HomeHomeGoodsSection"
@@ -8,18 +10,38 @@ import HomeGiftsSection from "../components/HomeGiftsSection"
 export default function Home() {
   return (
     <Layout>
-      <section>
-        <HomeWomensSection />
-      </section>
-      <section>
-        <HomeMensSection />
-      </section>
-      <section>
-        <HomeGiftsSection />
-      </section>
-      <section>
-        <HomeHomeGoodsSection />
-      </section>
+      <Link to={"/womens"}>
+        <section className={section}>
+          <div className={innerContainer}>
+            <span className={text}>WOMEN</span>
+          </div>
+          <HomeWomensSection />
+        </section>
+      </Link>
+      <Link to={"mens"}>
+        <section className={section}>
+          <div className={innerContainer}>
+            <span className={text}>MEN</span>
+          </div>
+          <HomeMensSection />
+        </section>
+      </Link>
+      <Link to={"/gifts"}>
+        <section className={section}>
+          <div className={innerContainer}>
+            <span className={text}>GIFTS</span>
+          </div>
+          <HomeGiftsSection />
+        </section>
+      </Link>
+      <Link to={"home-goods"}>
+        <section className={section}>
+          <div className={innerContainer}>
+            <span className={text}>HOME GOODS</span>
+          </div>
+          <HomeHomeGoodsSection />
+        </section>
+      </Link>
     </Layout>
   )
 }
