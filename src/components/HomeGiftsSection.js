c75c8ad191ab6f7ac967dd196788debf090b2d1b
Allan Viray <allang.v.dev@gmail.com>
2022-12-01 19:40:56 -0600

    Edit gifts section on home page
    - Resize and replace images.
    - Edit styles.

--- src/components/HomeGiftsSection.js
+++ src/components/HomeGiftsSection.js
@@ -2,39 +2,39 @@ import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import {
   sectionContent,
-  imageContainer,
+  mobileStaticImage,
+  desktopImageContainer,
 } from "../styles/home-gifts-section.module.css"
 import useWindowDimensions from "./hooks/useWindowDimensions"
 
 const HomeGiftsSection = () => {
   const windowDimensions = useWindowDimensions()
 
-  const mobileImage = "../departments/images/home-gifts-feat-img-mobile.jpg"
-  const desktopImage = "../departments/images/home-gifts-collage03.png"
+  const mobileImage = "../departments/images/home-gifts-mobile.jpg"
+  const desktopImage = "../departments/images/home-gifts-collage.png"
 
   return (
     <div className={sectionContent}>
-      <div>
-        {windowDimensions.width < 800 ? (
-          <>
-            <StaticImage
-              src={mobileImage}
-              alt="white handbag"
-              placeholder="blurred"
-              layout="fullWidth"
-            />
-          </>
-        ) : (
-          <div className={imageContainer}>
-            <StaticImage
-              src={desktopImage}
-              alt="collage of people and accessories"
-              placeholder="blurred"
-              layout="fullWidth"
-            />
-          </div>
-        )}
-      </div>
+      {windowDimensions.width < 800 ? (
+        <StaticImage
+          className={mobileStaticImage}
+          src={mobileImage}
+          alt="white handbag"
+          placeholder="blurred"
+          objectFit="cover"
+          objectPosition={"top"}
+        />
+      ) : (
+        <div className={desktopImageContainer}>
+          <StaticImage
+            src={desktopImage}
+            alt="collage of people and accessories"
+            placeholder="blurred"
+            objectFit="cover"
+            objectPosition={"50% 50%"}
+          />
+        </div>
+      )}
     </div>
   )
 }
