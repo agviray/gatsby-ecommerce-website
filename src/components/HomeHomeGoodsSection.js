327fa04dc0c40d20647a3110ff7b3800136da6d2
Allan Viray <allang.v.dev@gmail.com>
2022-12-01 22:33:09 -0600

    Edit home goods section on home page
    - Resize and replace images.
    - Edit styles.

--- src/components/HomeHomeGoodsSection.js
+++ src/components/HomeHomeGoodsSection.js
@@ -1,36 +1,59 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-import * as styles from "../styles/home-home-goods-section.module.css"
+import {
+  sectionContent,
+  mobileStaticImage,
+} from "../styles/home-home-goods-section.module.css"
 import useWindowDimensions from "./hooks/useWindowDimensions"
 
 const HomeHomeGoodsSection = () => {
   const windowDimensions = useWindowDimensions()
 
-  const mobileImage =
-    "../departments/images/home-home-goods-feat-img-mobile.jpg"
-  const desktopImage =
-    "../departments/images/home-home-goods-feature-img-desktop.jpg"
+  const mobileImage = "../departments/images/home-home-goods-mobile.jpg"
+  const desktopImage = "../departments/images/home-home-goods-desktop.jpg"
+
+  const renderedMobileImage = (
+    <>
+      <StaticImage
+        className={mobileStaticImage}
+        src={mobileImage}
+        alt="red chair sitting in nature"
+        placeholder="blurred"
+        objectFit="cover"
+        objectPosition={"top"}
+      />
+    </>
+  )
+
+  const renderedDesktopImage = (
+    <>
+      <StaticImage
+        className={`${
+          windowDimensions.width < 800 &&
+          windowDimensions.width > windowDimensions.height
+            ? mobileStaticImage
+            : null
+        }`}
+        src={desktopImage}
+        alt="red chair sitting in nature"
+        placeholder="blurred"
+        layout="fullWidth"
+      />
+    </>
+  )
+
+  const showMobileImage = () => {
+    return windowDimensions.width < windowDimensions.height
+      ? renderedMobileImage
+      : renderedDesktopImage
+  }
 
   return (
-    <div className={styles.sectionContent}>
+    <div className={sectionContent}>
       {windowDimensions.width < 800 ? (
-        <>
-          <StaticImage
-            src={mobileImage}
-            alt="red chair sitting in nature"
-            placeholder="blurred"
-            layout="fullWidth"
-          />
-        </>
+        <>{showMobileImage()}</>
       ) : (
-        <>
-          <StaticImage
-            src={desktopImage}
-            alt="red chair sitting in nature"
-            placeholder="blurred"
-            layout="fullWidth"
-          />
-        </>
+        <>{renderedDesktopImage}</>
       )}
     </div>
   )
