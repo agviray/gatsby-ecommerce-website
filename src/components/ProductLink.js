f974deaea1c92f26dd31b3f898ae067eb4de44c6
Allan Viray <allang.v.dev@gmail.com>
2023-01-08 19:22:08 -0600

    Apply initial styles to ProductLink.js

--- src/components/ProductLink.js
+++ src/components/ProductLink.js
@@ -1,14 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
+import {
+  container,
+  linkContents,
+  imageWrapper,
+  caption,
+  productName,
+  productPrice,
+} from "../styles/product-link.module.css"
 
 const ProductLink = ({ children, name, price, to }) => {
   return (
-    <div className="container">
+    <div className={container}>
       <Link to={to}>
-        <div className="image">{children}</div>
-        <div className="caption">
-          <h3>{name}</h3>
-          <span>{price}</span>
+        <div className={linkContents}>
+          <div className={imageWrapper}>{children}</div>
+          <div className={caption}>
+            <div className={productName}>{name}</div>
+            <div className={productPrice}>{price}</div>
+          </div>
         </div>
       </Link>
     </div>
