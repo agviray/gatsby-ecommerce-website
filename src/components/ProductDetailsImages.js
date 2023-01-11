4eb8670d3e346751afdb0dc90c609a087ca0a6fa
Allan Viray <allang.v.dev@gmail.com>
2023-01-10 19:31:25 -0600

    Apply styles to create a carousel structure

--- src/components/ProductDetailsImages.js
+++ src/components/ProductDetailsImages.js
@@ -2,33 +2,37 @@ import React, { useState, useEffect } from "react"
 import {
   container,
   content,
-  thumbnailCollection,
-  thumbnail,
+  imagesContainer,
+  imagesContent,
+  productImage,
   mainImage,
 } from "../styles/product-details-images.module.css"
+import useWindowDimensions from "./hooks/useWindowDimensions"
 
 const ProductDetailsImages = ({ allProductImages }) => {
   const [viewedImage, setViewedImage] = useState(allProductImages[0])
+  const windowDimensions = useWindowDimensions()
 
-  const renderThumbnails = images => {
-    const thumbnails = images.filter(
-      image => image.imageNumber !== viewedImage.imageNumber
-    )
-    return thumbnails.map((image, index) => (
-      <div key={index} className={thumbnail}>
-        {image.content()}
-      </div>
-    ))
-  }
+  // const renderThumbnails = images => {
+  //   const thumbnails = images.filter(
+  //     image => image.imageNumber !== viewedImage.imageNumber
+  //   )
+  //   return thumbnails.map((image, index) => (
+  //     <div key={index} className={productImage}>
+  //       {image.content()}
+  //     </div>
+  //   ))
+  // }
 
   return (
     <div className={container}>
       <div className={content}>
-        <div className={thumbnailCollection}>
-          {renderThumbnails(allProductImages)}
-        </div>
-        <div className={mainImage}>
-          <div className={thumbnail}>{viewedImage.content()}</div>
+        <div className={imagesContainer}>
+          <div className={imagesContent}>
+            {allProductImages.map((image, index) => (
+              <div key={index} className={productImage}>{image.content()}</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
