f77d6a6fd65e7eeb32363ffcdb2f3209f282804f
Allan Viray <allang.v.dev@gmail.com>
2023-01-13 18:33:43 -0600

    Add initial footer content

---
+++ src/components/FooterContent.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react"
+import twitterIcon from "../images/social-media/twitter-icon.svg"
+import instagramIcon from "../images/social-media/instagram-icon.svg"
+import facebookIcon from "../images/social-media/facebook-icon.svg"
+import {
+  container,
+  content,
+  contentBlock,
+  blockHeading,
+  companyInfo,
+  companyInfoContent,
+  socialMedia,
+  socialMediaContent,
+  item,
+  socialMediaIcon,
+  siteCopyright,
+  siteCopyrightContent,
+} from "../styles/footer-content.module.css"
+
+const socialMediaIcons = [
+  {
+    src: twitterIcon,
+    alt: "twitter icon",
+    href: "https://www.twitter.com",
+  },
+  {
+    src: instagramIcon,
+    alt: "instagram icon",
+    href: "https://www.instagram.com",
+  },
+  {
+    src: facebookIcon,
+    alt: "facebook icon",
+    href: "https://www.facebook.com",
+  },
+]
+
+const currentYear = new Date().getFullYear()
+
+const FooterContent = ({ title }) => {
+  return (
+    <div className={container}>
+      <div className={content}>
+        <div className={contentBlock}>
+          <h4 className={blockHeading}>MORE INFO</h4>
+          <div className={companyInfo}>
+            <ul className={companyInfoContent}>
+              <li className={item}>
+                <a href="#">Contact</a>
+              </li>
+              <li className={item}>
+                <a href="#">About</a>
+              </li>
+              <li className={item}>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={contentBlock}>
+          <h4 className={blockHeading}>GET CONNECTED</h4>
+          <div className={socialMedia}>
+            <ul className={socialMediaContent}>
+              {socialMediaIcons.map(icon => (
+                <li key={icon.alt} className={`${item} ${socialMediaIcon}`}>
+                  <a href={icon.href} target="_blank">
+                    <img src={icon.src} alt={icon.alt} />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className={contentBlock}>
+          <div className={siteCopyrightContent}>
+            &copy; {currentYear} {title}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FooterContent
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Navbar from "./Navbar"
+import FooterContent from "./FooterContent"
 import "../styles/global.css"
 
 export default function Layout({ children }) {
@@ -17,13 +18,13 @@ export default function Layout({ children }) {
   const { title } = data.site.siteMetadata
 
   return (
-    <div className="layout">
+    <div>
       <header>
         <Navbar />
       </header>
       <main>{children}</main>
       <footer>
-        <p>Copyright &copy; 2022 {title}</p>
+        <FooterContent title={title} />
       </footer>
     </div>
   )
