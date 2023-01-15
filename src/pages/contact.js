fda01388f02fd4c792552224f249c3f87bee96ea
Allan Viray <allang.v.dev@gmail.com>
2023-01-15 17:22:48 -0600

    Create styles file for Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,41 +1,69 @@
 import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout"
+import {
+  container,
+  heading,
+  content,
+  formContainer,
+  formHeading,
+  formContent,
+  formItem,
+  formLabel,
+  formTextInput,
+  formTextarea,
+} from "../styles/contact.module.css"
 
 const Contact = () => {
   return (
     <Layout>
-      <div className="container">
-        <div className="heading">
+      <div className={container}>
+        <div className={heading}>
           <h2>CONTACT</h2>
         </div>
-        <div className="content">
-          <div className="formContainer">
+        <div className={content}>
+          <div className={formContainer}>
             <form action="">
-              <h3 className="formHeading">MESSAGE US</h3>
-              <div className="formContent">
-                <div className="formItem">
-                  <label htmlFor="ContactFormName">NAME</label>
-                  <input type="text" id="ContactFormName" placeholder="NAME" />
-                </div>
-                <div className="formItem">
-                  <label htmlFor="ContactFormEmail">EMAIL</label>
+              <h3 className={formHeading}>MESSAGE US</h3>
+              <div className={formContent}>
+                <div className={formItem}>
+                  <label className={formLabel} htmlFor="ContactFormName">
+                    NAME
+                  </label>
                   <input
+                    className={formTextInput}
+                    type="text"
+                    id="ContactFormName"
+                    placeholder="NAME"
+                  />
+                </div>
+                <div className={formItem}>
+                  <label className={formLabel} htmlFor="ContactFormEmail">
+                    EMAIL
+                  </label>
+                  <input
+                    className={formTextInput}
                     type="text"
                     id="ContactFormEmail"
                     placeholder="EMAIL"
                   />
                 </div>
-                <div className="formItem">
-                  <label htmlFor="ContactFormPhone">PHONE</label>
+                <div className={formItem}>
+                  <label className={formLabel} htmlFor="ContactFormPhone">
+                    PHONE
+                  </label>
                   <input
+                    className={formTextInput}
                     type="text"
                     id="ContactFormPhone"
                     placeholder="PHONE"
                   />
                 </div>
-                <div className="formItem">
-                  <label htmlFor="ContactFormMessage">MESSAGE</label>
+                <div className={formItem}>
+                  <label className={formLabel} htmlFor="ContactFormMessage">
+                    MESSAGE
+                  </label>
                   <textarea
+                    className={formTextarea}
                     rows="15"
                     id="ContactFormMessage"
                     placeholder="MESSAGE"
