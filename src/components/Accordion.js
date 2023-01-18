edbfbbb1dad9247eda163012bec90fb05b5e2a32
Allan Viray <allang.v.dev@gmail.com>
2023-01-18 17:15:32 -0600

    Implement state to open or close each accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,24 +1,45 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import {
   accordion,
   headingContainer,
   arrowContainer,
   arrow,
+  arrowActive,
   bodyContainer,
+  bodyContent,
 } from "../styles/accordion.module.css"
 
 const Accordion = ({ accordionContent }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const bodyTextRef = useRef(null)
+
+  useEffect(() => {
+    const bodyText = bodyTextRef.current
+    const bodyTextScrollHeight = bodyText.scrollHeight
+
+    if (isOpen) {
+      bodyText.style.maxHeight = `${bodyTextScrollHeight}px`
+    }
+
+    if (!isOpen) {
+      bodyText.style.maxHeight = ``
+    }
+  }, [isOpen])
   const renderContent = content => {
     return (
       <div className={accordion}>
-        <div className={headingContainer}>
+        <div onClick={() => setIsOpen(!isOpen)} className={headingContainer}>
           <h3>{content.headingText}</h3>
           <div className={arrowContainer}>
-            <span className={arrow}></span>
+            <span
+              className={`${arrow} ${isOpen ? `${arrowActive}` : ""}`}
+            ></span>
           </div>
         </div>
-        <div className={bodyContainer}>
-          <p>{content.bodyText}</p>
+        <div ref={bodyTextRef} className={bodyContainer}>
+          <div className={bodyContent}>
+            <p>{content.bodyText}</p>
+          </div>
         </div>
       </div>
     )
