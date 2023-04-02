10aee1720d01efe747b14c2d10caf1003207d8b3
Allan Viray <allang.v.dev@gmail.com>
2023-04-02 17:34:05 -0500

    Add content and styles to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,36 @@
 import React from "react"
+import { Link } from "gatsby"
+import {
+  container,
+  heading,
+  subheading,
+  content,
+  siteLink,
+} from "../styles/404.module.css"
 import Layout from "../components/Layout"
 
 export default function NotFound() {
   return (
     <Layout>
-      <div>
-        <h1>404</h1>
-        <p>
-          Uh-oh! Looks like the page you are trying to access does not exist.
-        </p>
-      </div>
+      <section>
+        <div className={container}>
+          <h2 className={heading}>404</h2>
+          <h3 className={subheading}>PAGE NOT FOUND</h3>
+          <div className={content}>
+            <p>
+              Uh-oh! Looks like the page you are trying to access does not
+              exist.
+            </p>
+            <p>
+              Head back to our{" "}
+              <Link to="/">
+                <span className={siteLink}>Home page</span>
+              </Link>{" "}
+              to continue shopping.
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
